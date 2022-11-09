057e1dcb963cd944641d32950c95d90d13d090eb
diletta95 <didifiasco@gmail.com>
2022-11-09 17:03:30 +0100

    first commit files
    test function play

---
+++ src/test/java/myjenktest/fizzBuzzTest.java
@@ -0,0 +1,24 @@
+package myjenktest;
+
+import org.junit.jupiter.api.*;
+
+
+import myjenktest.tutorial.FizzBuzz;
+
+public class fizzBuzzTest {
+	public FizzBuzz fb;
+	
+	@BeforeEach
+	public void setup(){
+		fb= new FizzBuzz ();
+	}
+	
+	@DisplayName("Play FizzBuzz with number=1")
+	@Test
+	public void testNumber() {
+		String risultato=fb.play(1);
+		Assertions.assertEquals(risultato, "1");
+	}
+	
+		
+}

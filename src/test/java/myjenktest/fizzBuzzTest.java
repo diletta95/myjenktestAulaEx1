acb72b15d62ba09564c92515506d8007f61fa502
diletta95 <didifiasco@gmail.com>
2022-11-10 14:45:10 +0100

    Update fizzBuzz 1

--- src/test/java/myjenktest/fizzBuzzTest.java
+++ src/test/java/myjenktest/fizzBuzzTest.java
@@ -20,6 +20,12 @@ public class fizzBuzzTest {
 		String risultato=fb.play(1);
 		Assertions.assertEquals(risultato, "1");
 	}
+	@DisplayName("Play FizzBuzz with number=3")
+	@Test
+	public void testFizz() {
+		String risultato=fb.play(3);
+		Assertions.assertEquals(risultato,"Fizz");
+	}
 	
 		
 }
